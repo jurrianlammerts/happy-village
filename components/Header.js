9a578463de3d50871b4ca72dfe4cc4efafae0a2c
Jurrian <jurrianlammerts@hotmail.com>
2021-02-17 18:10:16 +0100

    Small fixes

--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,23 @@
+import Link from 'next/link';
 import { Cross as Hamburger } from 'hamburger-react';
 
 const Header = ({ handleMenu, menuOpen }) => (
   <header>
     <div className="header-inner">
-      <div className="header-inner-col left">
-        <div className="header-inner-logo">
-          <h2>lamb digital</h2>
-        </div>
+      <div className="header-inner-logo">
+        <Link href="/">lamb digital</Link>
       </div>
-      <div className="header-inner-col right">
-        <div className="header-inner-nav">
-          <div className="header-inner-nav-menu">
-            <span className="header-inner-nav-label">menu</span>
-            <div className="header-inner-nav-burger" id="hover-link">
-              <Hamburger
-                id="hover-link"
-                toggled={menuOpen}
-                toggle={handleMenu}
-                size={24}
-                duration={0.2}
-                label="Show menu"
-              />
-            </div>
-          </div>
+      <div className="header-inner-menu">
+        <span className="header-inner-menu-label">menu</span>
+        <div className="header-inner-menu-burger" id="hover-link">
+          <Hamburger
+            id="hover-link"
+            toggled={menuOpen}
+            toggle={handleMenu}
+            size={24}
+            duration={0.2}
+            label="Show menu"
+          />
         </div>
       </div>
     </div>
