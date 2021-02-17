03e9a8375c9e72fa99e1fa593ea5c3fd278248ce
Jurrian <jurrianlammerts@hotmail.com>
2021-02-17 12:45:40 +0100

    Added meta description

--- components/SEO.js
+++ components/SEO.js
@@ -5,6 +5,10 @@ const SEO = ({ pageTitle }) => (
   <Head>
     <title>happy village • Leading digital agency</title>
     <link rel="icon" href="/favicon.ico" />
+    <meta
+      name="description"
+      content="Leading digital agency focused on UI/UX design. We develop complex websites, iOS/Android mobile applications and elaborate online business services."
+    />
   </Head>
 );
 
